2f92bb7507eb9339b2df59b52590b42fe5311c7e
Rulfy <git@zynox.net>
2014-04-19 18:58:19 +0200

    Updated and fixed SideMessage.lua and added a small example.

--- Libraries/SideMessage.lua
+++ Libraries/SideMessage.lua
@@ -7,96 +7,129 @@
  0 1 1 0 1 1 0 0             /_/        /___/             
  0 1 1 0 0 0 0 1    
  0 1 1 1 1 0 0 0  
+
+
+	Functions:
+		msg = sideMessage:CreateMessage(w,h[,bgColor,bdColor,
+											enterTime,stayTime,exitTime]): 	
+													Creates a new sideMessage object with the given width and height.
+													You may also give an optional frame and background color.
+													The last 3 parameters are used for the timings of the side message.
+													After the exit time, the message is automaticalle destroyed.
+
+	msg:AddElement(drawObject):						Add any draw object to the newly created side message. 
+													The position of the draw object will be used as an relative position!
+													Please only add elements after freshly creating a message, since it may become invalid after the exitTime passed.
+
+	Attributes (please don't change them):
+		sideMessage.ENTER_TIME: default enter time is 650 ms.
+		sideMessage.STAY_TIME:	default stay time is 2500 ms.
+		sideMessage.EXIT_TIME: 	default exit time is 650 ms.
+
+		sideMessage.bgColor: 	default color of the background.
+		sideMessage.bdColor:	default color of the frame.
+															
+	Example:
+		do
+			local myFont = drawMgr:CreateFont("sideMsg","Arial",14,10) 
+			local msg = sideMessage:CreateMessage(300,40)
+			msg:AddElement( drawMgr:CreateText(5,5,-1,"Hello world",myFont) )
+			msg:AddElement( drawMgr:CreateText(5,20,-1,"The game is already running "..math.floor(client.gameTime) .." seconds",myFont) )
+		end
  ]]
 
 
 sideMessage = {}
 
-sideMessage.id = 0
 sideMessage.lastTick = nil
 sideMessage.objs = {}
 
 sideMessage.ENTER_TIME = 650
 sideMessage.STAY_TIME = 2500
 sideMessage.EXIT_TIME = 650
+sideMessage.bgColor = 0x111111C0
+sideMessage.bdColor = 0x444444FF
 
-function sideMessage:CreateMessage(w,h,bgColor,bdColor)
-	if not bgColor then bgColor = 0x111111C0 end
-	if not bdColor then bdColor = 0x444444FF end
-	obj = {}
-	obj.w = w
-	obj.h = h
-	obj.bgColor = bgColor
-	obj.bdColor = bdColor
- 	obj.id = sideMessage.id
- 	sideMessage.id = sideMessage.id + 1
- 	obj.bg = drawMgr:CreateRect(client.screenSize.x,client.screenSize.y*0.64,w,h,bgColor)
- 	obj.bd = drawMgr:CreateRect(client.screenSize.x,client.screenSize.y*0.64,w,h,bdColor,true)
- 	obj.createTick = GetTick()
- 	obj.elements = {}
+function sideMessage:CreateMessage(pw,ph,pbgColor,pbdColor,penterTime,pstayTime,pexitTime)
+	if not pbgColor then pbgColor = self.bgColor end
+	if not pbdColor then pbdColor = self.bdColor end
+	if not penterTime then penterTime = self.ENTER_TIME end
+	if not pstayTime then pstayTime = self.STAY_TIME end
+	if not pexitTime then pexitTime = self.EXIT_TIME end
 
- 	function obj:Destroy()
- 		self.bg.visible = false
- 		self.bg = nil
- 		self.bd.visible = false
- 		self.bd = nil
- 		for i,v in ipairs(self.elements) do
- 			v.obj.visible = false
- 			v.obj = nil
- 		end
- 		sideMessage.objs[self.id] = nil
- 		self = nil
- 	end
+	local screenSize = client.screenSize
+	local selfMsg = self
+	local obj = {
+		w = pw, h = ph,
+		bgColor = pbgColor, bdColor = pbdColor,
+		bg = drawMgr:CreateRect(screenSize.x,screenSize.y*0.64,pw,ph,pbgColor),
+ 		bd = drawMgr:CreateRect(screenSize.x,screenSize.y*0.64,pw,ph,pbdColor,true),
+ 		enterTime = penterTime, stayTime = pstayTime, exitTime = pexitTime,
+ 		createTick = GetTick(),
+ 		elements = {},
+	 		Destroy = 	function(self)
+	 						self.bg.visible = false
+					 		self.bg = nil
+					 		self.bd.visible = false
+					 		self.bd = nil
+					 		for _,v in ipairs(self.elements) do
+					 			v.obj.visible = false
+					 			v.obj = nil
+					 		end
+	 					end,
+ 		AddElement = 	function(self,drawObj)
+ 							table.insert(self.elements, {obj=drawObj,pos=drawObj.position})
+				 			drawObj.position = drawObj.position + self.bg.position
+				 		end,
+		ShiftVec =	 	function(self,vec)
+					 		self.bg.position = self.bg.position + vec
+					 		self.bd.position = self.bd.position + vec
+					 		for _,v in ipairs(self.elements) do
+					 			v.obj.position = v.obj.position + vec
+					 		end
+ 						end,
+ 		SetX =			function(self,x)
+					 		self.bg.x = x
+					 		self.bd.x = x
+					 		for _,v in ipairs(self.elements) do
+					 			v.obj.x = v.pos.x + x
+					 		end
+					 	end
 
- 	function obj:AddElement(drawObj,relPos)
- 		self.elements[#self.elements + 1] = {obj = drawObj,relPos = relPos}
- 		drawObj.position = self.bg.position + relPos
- 	end
+	}
 
- 	function obj:ShiftVec(vec)
- 		self.bg.position = self.bg.position + vec
- 		self.bd.position = self.bd.position + vec
- 		for i,v in ipairs(self.elements) do
- 			v.obj.position = v.obj.position + vec
- 		end
- 	end
-
- 	function obj:SetX(x)
- 		self.bg.x = x
- 		self.bd.x = x
- 		for i,v in ipairs(self.elements) do
- 			v.obj.x = v.relPos.x + x
- 		end
- 	end
-
- 	for i,v in pairs(sideMessage.objs) do
+ 	for _,v in ipairs(self.objs) do
 	 	if v then
-	 		v:ShiftVec(Vector2D(0,-h - 3))
+	 		v:ShiftVec(Vector2D(0,-ph - 3))
 	 	end
 	 end
-
- 	sideMessage.objs[obj.id] = obj
+	table.insert(self.objs,obj)
  	return obj
 end
 
- function sideMessage_Frame()
- 	if sideMessage.lastTick then
-	 	for i,v in pairs(sideMessage.objs) do
+ function sideMessage:Frame(tick)
+ 	if self.lastTick then
+ 		local remove = {}
+	 	for i,v in ipairs(self.objs) do
 	 		if v then
 	 			local span = GetTick() - v.createTick
-	 			if span < sideMessage.ENTER_TIME then
-	 				v:SetX(client.screenSize.x - (v.w-1)*span/sideMessage.ENTER_TIME)
-	 			elseif span < sideMessage.ENTER_TIME + sideMessage.STAY_TIME then
+	 			if span < v.enterTime then
+	 				v:SetX(client.screenSize.x - (v.w-1)*span/v.enterTime)
+	 			elseif span < v.enterTime + v.stayTime then
 	 				v:SetX(client.screenSize.x - v.w + 1)
-	 			elseif span < sideMessage.ENTER_TIME + sideMessage.STAY_TIME + sideMessage.EXIT_TIME then
-	 				v:SetX(client.screenSize.x - (v.w-1)*(sideMessage.ENTER_TIME + sideMessage.STAY_TIME + sideMessage.EXIT_TIME - span)/sideMessage.EXIT_TIME)
+	 			elseif span < v.enterTime + v.stayTime + v.exitTime then
+	 				v:SetX(client.screenSize.x - (v.w-1)*(v.enterTime + v.stayTime + v.exitTime - span)/v.exitTime)
 	 			else
 	 				v:Destroy()
+	 				table.insert(remove,i)
 	 			end
 	 		end
 	 	end
+	 	for i,v in ipairs(remove) do
+	 		table.remove(self.objs,v)
+	 	end
 	end
-	sideMessage.lastTick = GetTick()
+	self.lastTick = GetTick()
  end
 
- scriptEngine:RegisterLibEvent(EVENT_FRAME,sideMessage_Frame)
+ scriptEngine:RegisterLibEvent(EVENT_FRAME,sideMessage.Frame,sideMessage)
