e5fcd6f9d7f16baf6563c8521730186c58fdafae
Rulfy <git@zynox.net>
2014-04-19 15:11:02 +0200

    found newer VectorOp.lua from Sophylax

--- Libraries/VectorOp.lua
+++ Libraries/VectorOp.lua
@@ -1,48 +1,89 @@
 --[[
+ 0 1 0 1 0 0 1 1    
+ 0 1 1 0 1 1 1 1        ____          __        __         
+ 0 1 1 1 0 0 0 0       / __/__  ___  / /  __ __/ /__ ___ __
+ 0 1 1 0 1 0 0 0      _\ \/ _ \/ _ \/ _ \/ // / / _ `/\ \ /
+ 0 1 1 1 1 0 0 1     /___/\___/ .__/_//_/\_, /_/\_,_//_\_\ 
+ 0 1 1 0 1 1 0 0             /_/        /___/             
+ 0 1 1 0 0 0 0 1    
+ 0 1 1 1 1 0 0 0    
+
+
 		Save as VectorOp.lua into Ensage\Scripts\libs.
 
 		Functions:
-			vectorOp:Length3D(Vector) : Returns the 3D Length of the vector
-			vectorOp:Length2D(Vector) : Returrs the 2D (x,y) Length of the vector
-			vectorOp:Clone(Vector) : Returns a copy of the vector
-			vectorOp:Unit2D(Vector) : Returns the unit vector of the given vector on XY plane
-			vectorOp:Unit3D(Vector) : Returns the unit vector of the given vector
-			vectorOp:GetXYAngle(Vector) : Returns the angle of the vector on XY plane
-			vectorOp:UnitVectorFromXYAngle(Angle) : Returns a unit vector from given angle
-			vectorOp:tostring(Vector) : Returns the coordinates of the vector in string form
+			Vector:Clone(): Retruns a clone of the given Vector for copying purposes.
+
+			Vector2D:Clone(): Retruns a clone of the given Vector2D for copying purposes.
+
+			Vector:Unit(): Returns the unit Vector that is on  the same xy direction as the given Vector.
+
+			Vector2D:Unit(): Returns the unit Vector2D that is on  the same xy direction as the given Vector2D.
+
+			Vector:GetXYAngle(): Returns the XY angle of the given Vector.
+
+			Vector2D:GetXYAngle(): Returns the XY angle of the given Vector2D.
+
+			VectorOp.UnitVectorFromXYAngle(alpha): Creates and returns a unit Vector with given angle as its XY angle.
+
+			VectorOp.UnitVector2DFromXYAngle(alpha): Creates and returns a unit Vector2D with given angle as its XY angle.
+
+			Vector:GetDistance2D(a): Calculates the distance between the given Vector and parameter.
+
+			Vector2D:GetDistance2D(a): Calculates the distance between the given Vector2D and parameter.
+
 --]]
 
-vectorOp = {}
+VectorOp = {}
 
-function vectorOp:Length3D(v)
-	return math.sqrt(math.pow(v.x,2)+math.pow(v.y,2)+math.pow(v.z,2))
+function Vector:Clone()
+	return Vector(self.x,self.y,self.z)
 end
 
-function vectorOp:Length2D(v)
-	return math.sqrt(math.pow(v.x,2)+math.pow(v.y,2)+math.pow(v.z,2))
+function Vector2D:Clone()
+	return Vector2D(self.x,self.y)
 end
 
-function vectorOp:Clone(v)
-	return Vector(v.x,v.y,v.z)
+function Vector:Unit()
+	local self = self/#self
+	return Vector(self.x,self.y,0)
 end
 
-function vectorOp:Unit3D(v)
-	return v/vectorOp:Length3D(v)
+function Vector2D:Unit()
+	local self = self/#self
+	return Vector2D(self.x,self.y)
 end
 
-function vectorOp:Unit2D(v)
-	local v = v/vectorOp:Length2D(v)
-	return Vector(v.x,v.y,0)
+function Vector:GetXYAngle()
+	return math.atan2(self.y,self.x)
 end
 
-function vectorOp:GetXYAngle(v)
-	return math.atan2(v.y,v.x)
+function Vector2D:GetXYAngle()
+	return math.atan2(self.y,self.x)
 end
 
-function vectorOp:UnitVectorFromXYAngle(alpha)
+function VectorOp.UnitVectorFromXYAngle(alpha)
 	return Vector(math.cos(alpha),math.sin(alpha),0)
 end
 
-function vectorOp:tostring(v)
-	return "("..v.x..","..v.y..","..v.z..")"
+function VectorOp.UnitVector2DFromXYAngle(alpha)
+	return Vector2D(math.cos(alpha),math.sin(alpha))
+end
+
+function Vector:GetDistance2D(a)
+	assert(GetType(a) == "Vector" or GetType(a) == "LuaEntity" or GetType(a) == "Vector2D" or GetType(a) == "Projectile", "GetDistance2D: Invalid Parameter (Got "..GetType(a)..")")
+	if a.x == nil or a.y == nil then
+		return self:GetDistance2D(a.position)
+	else
+		return math.sqrt(math.pow(a.x-self.x,2)+math.pow(a.y-self.y,2))
+	end
+end
+
+function Vector2D:GetDistance2D(a)
+	assert(GetType(a) == "Vector" or GetType(a) == "LuaEntity" or GetType(a) == "Vector2D" or GetType(a) == "Projectile", "GetDistance2D: Invalid Parameter (Got "..GetType(a)..")")
+	if a.x == nil or a.y == nil then
+		return self:GetDistance2D(a.position)
+	else
+		return math.sqrt(math.pow(a.x-self.x,2)+math.pow(a.y-self.y,2))
+	end
 end
